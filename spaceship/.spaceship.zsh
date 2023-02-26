9da2c7f92373291a049f523fb60dcafdc6a5a6b8
hiramekun <thescript1210@gmail.com>
2023-02-26 13:16:56 +0900

    add spaceship config

---
+++ spaceship/.spaceship.zsh
@@ -0,0 +1,17 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+# Add custom Ember section
+# See: https://github.com/spaceship-prompt/spaceship-ember
+
+SPACESHIP_PROMPT_SYMBOL="➜"
+SPACESHIP_PROMPT_ADD_NEWLINE=true
+SPACESHIP_PROMPT_SEPARATE_LINE=true
+SPACESHIP_PROMPT_PREFIXES_SHOW=true
+SPACESHIP_PROMPT_SUFFIXES_SHOW=true
