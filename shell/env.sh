aeb774d369c0422dfd448f70f7dc57f94c88501e
hiramekun <thescript1210@gmail.com>
2023-08-23 23:38:09 +0900

    Add GOPATH

--- shell/env.sh
+++ shell/env.sh
@@ -1,8 +1,10 @@
 export HOMEBREW_PREFIX="/opt/homebrew";
 export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
 export HOMEBREW_REPOSITORY="/opt/homebrew";
+export GOPATH=$(go env GOPATH)
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export PATH="$PATH:$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+export PATH="$PATH:$GOPATH/bin"
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
 export LSCOLORS=cxfxcxdxbxegedabagacad
