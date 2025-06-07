b62fa3df85eeb06dd4f63e1e518247f7111df9d9
Takaaki Hirano <thescript1210@gmail.com>
2025-06-08 01:57:50 +0900

    Revert "Add dry run tests for installer"

--- provisioning/run.sh
+++ provisioning/run.sh
@@ -8,31 +8,13 @@ cd "$(dirname $0)"
 RTUN='reattach-to-user-namespace'
 
 CMD=('ansible-playbook')
-
-if command -v ansible-playbook > /dev/null 2>&1; then
-  if [ -n "${PROVISION_DRY_RUN:-}" ]; then
-    CMD+=(--syntax-check)
-    echo 'DRYRUN: ansible-playbook --syntax-check'
-  fi
-else
-  echo 'skip (ansible-playbook not found)'
-  exit 0
-fi
-
-ARGS=()
+ARGS=('')
 for a in "$@"; do
-  ARGS+=("$a")
+  ARGS=(${ARGS[@]} "$a")
 done
 
-if [ -n "${TMUX:-}" ] && command -v "$RTUN" > /dev/null 2>&1; then
-  CMD=("$RTUN" "${CMD[@]}")
-fi
-
-if [ -z "${PROVISION_DRY_RUN:-}" ]; then
-  if ! command -v ansible-playbook > /dev/null 2>&1; then
-    echo 'skip (ansible-playbook not found)'
-    exit 0
-  fi
+if [ -n "${TMUX:-}" ] && command -v $RTUN > /dev/null 2>&1; then
+  CMD=($RTUN ${CMD[@]})
 fi
 
 ${CMD[@]} \
